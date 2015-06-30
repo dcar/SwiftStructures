3bf426f7d91a0347304318be189f15584302c396
Dom <ambient.crusader@gmail.com>
2015-06-30 03:42:14 -0500

    Added BitVector and tests

---
+++ Source/HashFunctions/HashFunctions.swift
@@ -0,0 +1,16 @@
+//
+//  HashFunctions.swift
+//  SwiftStructures
+//
+//  Created by Dominick Carro on 6/30/15.
+//  Copyright (c) 2015 Arbutus Software Inc. All rights reserved.
+//
+
+import Foundation
+
+struct HashFunctions {
+  static func murmur32(seed: Int, str: String) -> UInt32 {
+    var len = CInt(count(str))
+    return MurmurHashAligned2(str, len, CUnsignedInt(seed))
+  }
+}
---
+++ SwiftTests/BitVectorTest.swift
@@ -0,0 +1,9 @@
+//
+//  BitVectorTest.swift
+//  SwiftStructures
+//
+//  Created by Dominick Carro on 6/30/15.
+//  Copyright (c) 2015 Arbutus Software Inc. All rights reserved.
+//
+
+import Foundation
