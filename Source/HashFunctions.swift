2697531ec55720b195f0d77ee1b5dc7949dc3e83
Dom <ambient.crusader@gmail.com>
2015-06-30 03:46:06 -0500

    refactor

--- Source/HashFunctions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HashFunctions.swift
-//  SwiftStructures
-//
-//  Created by Dominick Carro on 6/30/15.
-//  Copyright (c) 2015 Arbutus Software Inc. All rights reserved.
-//
-
-import Foundation
