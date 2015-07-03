be94664175b40086f354853a080c18465dc5d283
Dom <ambient.crusader@gmail.com>
2015-07-03 03:20:43 -0500

    Updated test

--- SwiftTests/BitVectorTest.swift
+++ SwiftTests/BitVectorTest.swift
@@ -10,26 +10,36 @@ import UIKit
 import XCTest
 
 class BitVectorTest: XCTestCase {
-  var bitVector = BitVector(size: 10000)
-  var bitFile = BitFile(size: 10, location: "/Users/Dom/bit-file.dat")!
+  var bitVector: BitVector?
+  var bitFile: BitFile?
+  
+  override func setUp() {
+    super.setUp()
+    bitVector = BitVector(size: 10000)
+    bitFile = BitFile(size: 1000, location: "/Users/Dom/bit-file.dat")!
+  }
   
   func testFalse() {
-    bitVector[5] = true
-    bitVector[5] = false
-    var value = bitVector[5]
+    bitVector?[5] = true
+    bitVector?[5] = false
+    var value = bitVector?[5]
     XCTAssert(value == false, "value Should be false.")
   }
   
   func testTrue() {
-    bitVector[0] = true
-    var value = bitVector[0]
+    bitVector?[0] = true
+    var value = bitVector?[0]
     XCTAssert(value == true, "value Should be true.")
   }
   
   func testBitFileTrue() {
-    bitFile[8] = true
-    var value = bitFile[8]
+    bitFile?[545] = true
+    var value = bitFile?[545]
     XCTAssert(value == true, "value Should be true.")
+    bitFile?[544] = true
+    value = bitFile?[544]
+    XCTAssert(value == true, "value Should be false.")
   }
+
 
 }
